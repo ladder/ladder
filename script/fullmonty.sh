320e5c04f2ba8bfcc1c48f57ee89755c423588d5
MJ Suhonos <mj@suhonos.ca>
2012-09-01 10:25:07 -0400

    add merge task to script

--- script/fullmonty.sh
+++ script/fullmonty.sh
@@ -5,5 +5,6 @@ time padrino rake marc:import[~/Downloads/tmp/]	# import new MARC data
 time padrino rake marc:map		                # XSLT transform
 time padrino rake mods:map		                # map XML to vocabs
 time padrino rake tire:index	                # send mongodb data to ES index
+#time padrino rake tree:merge					# merge duplicate documents
 time padrino rake tree:build					# generate relation hierarchy
 time padrino rake tire:index	                # send mongodb data to ES index
