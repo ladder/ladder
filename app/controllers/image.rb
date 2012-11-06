d4c5c96439721704e34bf2def431bb70e876c2ce
MJ Suhonos <mj@suhonos.ca>
2012-11-05 20:03:54 -0500

    Add multi-API search for ISBN images

--- app/controllers/image.rb
+++ app/controllers/image.rb
@@ -6,12 +6,39 @@ Ladder.controllers :image do
 
     @resource = Resource.find(params[:id])
 
-    isbn = @resource.bibo.isbn.first.gsub(/[^0-9]/i, '') rescue nil
-    if isbn.nil?
-      # send an empty 1x1 gif.  or something.
-    end
+    # do lookup on each ISBN in order
+    isbns = @resource.bibo.isbn
 
-    data = open("http://covers.openlibrary.org/b/isbn/#{isbn}-#{size}.jpg")
+    while !isbns.empty?
+      # get a numeric-only ISBN to look up
+      isbn = isbns.pop.gsub(/[^0-9]/i, '')
+
+      # try syndetics
+      data = open("http://syndetics.com/index.aspx?isbn=#{isbn}/#{size}C.gif")
+      break if !data.nil? and data.meta['content-length'].to_i > 200
+
+      # try librarything
+      # TODO: add sizing
+      data = open("http://covers.librarything.com/devkey/KEY/medium/isbn/#{isbn}")
+      break if !data.nil? and data.meta['content-length'].to_i > 200
+
+      # try amazon
+      # TODO: add sizing
+      data = open("http://images.amazon.com/images/P/#{isbn}.01.MZZZZZZZ.jpg")
+      break if !data.nil? and data.meta['content-length'].to_i > 200
+
+      # try openlibrary
+      data = open("http://covers.openlibrary.org/b/isbn/#{isbn}-#{size}.jpg")
+      break if !data.nil? and data.meta['content-length'].to_i > 900
+
+      if isbns.empty?
+        # couldn't find any covers, send an empty 1x1 GIF
+        content_type 'image/gif'
+        body Base64.decode64('R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==')
+        halt 200
+      end
+
+    end
 
     unless data.nil?
       content_type data.content_type
