e1b90085322419d37a86394606a060fe1cd516a0
MJ Suhonos <mj@suhonos.ca>
2012-11-09 16:08:56 -0500

    Multi-thread image lookup

--- app/controllers/image.rb
+++ app/controllers/image.rb
@@ -9,42 +9,28 @@ Ladder.controllers :image do
     isbns = @resource.bibo.isbn rescue nil
 
     # do lookup on each ISBN in order
-    # TODO: background this somehow; Parallel.fork, Sidekiq, etc.
+    # TODO: move this off to Sidekiq or something
 
-    while !isbns.nil? and !isbns.empty?
+    thumb = nil
+    while !isbns.nil? and !isbns.empty? and thumb.nil?
       # get a numeric-only ISBN to look up
       isbn = isbns.pop.gsub(/[^0-9]/i, '')
 
-      # try syndetics
-      data = open("http://syndetics.com/index.aspx?isbn=#{isbn}/#{size}C.gif")
-      break if !data.nil? and data.meta['content-length'].to_i > 200
+      services = ["http://syndetics.com/index.aspx?isbn=#{isbn}/#{size}C.gif",
+                  "http://covers.librarything.com/devkey/KEY/medium/isbn/#{isbn}",
+                  "http://images.amazon.com/images/P/#{isbn}.01.MZZZZZZZ.jpg",
+                  "http://covers.openlibrary.org/b/isbn/#{isbn}-#{size}.jpg"]
 
-      # try librarything
-      # TODO: add sizing
-      data = open("http://covers.librarything.com/devkey/KEY/medium/isbn/#{isbn}")
-      break if !data.nil? and data.meta['content-length'].to_i > 200
-
-      # try amazon
-      # TODO: add sizing
-      data = open("http://images.amazon.com/images/P/#{isbn}.01.MZZZZZZZ.jpg")
-      break if !data.nil? and data.meta['content-length'].to_i > 200
-
-      # try openlibrary
-      data = open("http://covers.openlibrary.org/b/isbn/#{isbn}-#{size}.jpg")
-      break if !data.nil? and data.meta['content-length'].to_i > 900
-
-      if isbns.empty?
-        # couldn't find any covers, send an empty 1x1 GIF
-        content_type 'image/gif'
-        body Base64.decode64('R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==')
-        halt 200
+      Parallel.each(services, {:in_threads => services.size}) do |service|
+        data = open(service) if thumb.nil?
+        thumb = data if !data.nil? and data.meta['content-length'].to_i > 900
       end
 
     end
 
-    unless data.nil?
-      content_type data.content_type
-      body data.read
+    unless thumb.nil?
+      content_type thumb.content_type
+      body thumb.read
     else
       # send an empty 1x1 GIF
       content_type 'image/gif'
