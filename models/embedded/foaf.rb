e6bb698f0b5e58799faeadadbf112a02c06b8f1f
MJ Suhonos <mj@suhonos.ca>
2013-02-14 13:27:02 -0500

    Enable history tracking for all models

--- models/embedded/foaf.rb
+++ models/embedded/foaf.rb
@@ -1,5 +1,9 @@
 class FOAF
   include Model::Embedded
+
   bind_to RDF::FOAF, :type => Array, :localize => true, :only => [:name, :birthday, :title]
+
   embedded_in :agent
+
+  track_history :on => RDF::FOAF.properties
 end
--- models/embedded/prism.rb
+++ models/embedded/prism.rb
@@ -1,5 +1,9 @@
 class Prism
   include Model::Embedded
+
   bind_to Vocab::Prism, :type => Array, :localize => true, :only => [:edition, :hasPreviousVersion, :issueIdentifier]
+
   embedded_in :resource
+
+  track_history :on => Vocab::Prism.properties
 end
