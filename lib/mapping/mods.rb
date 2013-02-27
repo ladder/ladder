5f299a2819df066f74351f669aafe26d3c76eae8
MJ Suhonos <mj@suhonos.ca>
2013-02-27 16:20:15 -0500

    More fixed to rdf_types in image partial; update MODS mapping

--- lib/mapping/mods.rb
+++ lib/mapping/mods.rb
@@ -102,19 +102,17 @@ module Mapping
           # indexable textual content
           :abstract        => 'abstract',
           :tableOfContents => 'tableOfContents',
-
-          # TODO: add <note> etc.
       }
 
       bibo = map_xpath node, {
           # dereferenceable identifiers
-          :isbn     => 'identifier[@type = "isbn"]',
-          :issn     => 'identifier[@type = "issn"]',
-          :lccn     => 'identifier[@type = "lccn"]',
-          :oclcnum  => 'identifier[@type = "oclc"]',
-          :upc      => 'identifier[@type = "upc"]',
-          :doi      => 'identifier[@type = "doi"]',
-          :uri      => 'identifier[@type = "uri"]',
+          :isbn     => 'identifier[@type = "isbn" and not(@invalid)]',
+          :issn     => 'identifier[@type = "issn" and not(@invalid)]',
+          :lccn     => 'identifier[@type = "lccn" and not(@invalid)]',
+          :oclcnum  => 'identifier[@type = "oclc" and not(@invalid)]',
+          :upc      => 'identifier[@type = "upc" and not(@invalid)]',
+          :doi      => 'identifier[@type = "doi" and not(@invalid)]',
+          :uri      => 'identifier[@type = "uri" and not(@invalid)]',
       }
 
       prism = map_xpath node, {
@@ -122,9 +120,18 @@ module Mapping
           :issueIdentifier  => 'identifier[@type = "issue-number" or @type = "issue number"]',
       }
 
+      mods = map_xpath node, {
+          :note               => 'note',
+          :accessCondition    => 'accessCondition',
+          :issuance           => 'originInfo/issuance',
+          :frequency          => 'originInfo/frequency',
+          :locationOfResource => 'location',
+      }
+
       vocabs[:dcterms] = dcterms unless dcterms.empty?
-      vocabs[:bibo] = bibo unless bibo.empty?
-      vocabs[:prism] = prism unless prism.empty?
+      vocabs[:bibo]    = bibo    unless bibo.empty?
+      vocabs[:prism]   = prism   unless prism.empty?
+      vocabs[:mods]    = mods    unless mods.empty?
 
       vocabs
     end
