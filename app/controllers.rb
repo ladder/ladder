7567c8a8997575d552c0251957959c90ea6a2816
MJ Suhonos <mj@suhonos.ca>
2013-03-21 14:46:30 -0400

    Add DELETE method on root URI to replace rake tasks

--- app/controllers.rb
+++ app/controllers.rb
@@ -1,15 +1,35 @@
 Ladder.controllers  do
+  provides :json
 
-  get :index, :provides => :json do
+  get :index do
     # TODO: this might be a good place for ROAR / hypermedia links
-    status 200 # this is assumed
-    content_type 'json' # just in case
 
+    content_type :json
     {:name => 'Ladder',
      :ok => true,
      :status => 200,
      :version => '0.4',
      :tagline => 'Born in a library, raised on the Semantic Web'}.to_json
+  end
+
+  delete :index do
+    # Remove existing Mongo DB and ES index
+    Mongoid::Config.purge!
+
+    index_name = Mongoid::Sessions.default.options[:database]
+    index = Tire::Index.new(index_name)
+    index.delete
+
+    # Re-map indexes
+    %w[Agent Concept Resource].each do |model|
+      klass = model.classify.constantize
+      klass.create_indexes
+      klass.put_mapping
+    end
+
+    content_type :json
+    status index.response.code
+    body index.response.body
   end
 
 end
---
+++ app/helpers.rb
@@ -0,0 +1,8 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+=begin
+Ladder.helpers do
+  def current_url
+    url_for(request.path_info)
+  end
+end
+=end
