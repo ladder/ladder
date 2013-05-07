1a45212cce7fb767f7e809f24eac5df9e67c261e
mjsuhonos <mj@suhonos.ca>
2013-05-07 16:59:59 -0400

    Add POST endpoint for Mapping definitions

--- app/controllers/mapping.rb
+++ app/controllers/mapping.rb
@@ -28,4 +28,27 @@ Ladder.controllers :mappings do
     body({:ok => true, :status => 200}.to_json)
   end
 
+  # Upload a mapping hash in JSON
+  post :index do
+    # ensure we have content to process
+    halt 400, {:error => 'No content provided', :status => 400}.to_json if 0 == request.body.length
+
+    # parse provided JSON and trap any errors
+    begin
+      mapping_hash = JSON.parse(request.body.read)
+      mapping_hash = mapping_hash.symbolize_keys_recursive.slice(:type, :content_type, :vocabs, :agents, :concepts, :resources)
+
+      # TODO: use Mongoid #upsert functionality
+      @mapping = Mapping.create!(mapping_hash)
+
+    rescue Exception => error
+      halt 422, {:status => 422, :error => error.to_s}.to_json
+    end
+
+    status 201 # resource created
+
+    @opts[:all_keys] = true
+    render 'mapping', :format => :json
+  end
+
 end
