cb495315f85ba3e0925aa82b2ad408e74eb77811
Grant McInnes <gmcinnes@marsdd.com>
2013-09-04 13:18:39 -0400

    Add some more config options to make sure we pick up rbenv inside capistrano runs on the remote machine'

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,11 @@ set :ssh_options, { :forward_agent => true }
 set :user, "deployer"  # The server's user for deploys
 set :scm_passphrase, "p@ssw0rd"  # The deploy user's password
 set :deploy_via, :remote_cache
+set :deploy_to, "/home/deploy"
 
+set :default_environment, {
+  'PATH' => "/opt/rbenv/shims/:$PATH"
+}
 set :stages, %w(production development)
 set :default_stage, "development"
 require 'capistrano/ext/multistage'
