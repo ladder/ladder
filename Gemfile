abe95ffeeeee5ac16a66cbbde2b6d17ff391e2ab
mjsuhonos <mj@suhonos.ca>
2014-04-21 20:47:00 -0400

    Controller stubs for testing LD serialization

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem 'padrino', '~> 0.12.1'
 # end
 
 # Linked Data gems
-gem 'rack-linkeddata', '~> 1.1.0'
+gem 'sinatra-linkeddata', '~> 1.1.0'
 gem 'json-ld', '~> 1.1.2', require: 'json/ld'
 
 # Persistence gems
--- app/controllers.rb
+++ app/controllers.rb
@@ -1,11 +1,23 @@
 L2::App.controllers  do
 
 #  register Sinatra::LinkedData
-  use Rack::LinkedData::ContentNegotiation, :standard_prefixes => true
+  use Rack::LinkedData::ContentNegotiation, standard_prefixes: true
 
   get :index do
     r = Resource.new ; r.dc.title = 'title' ; I18n.locale = :fr ; r.dc.title = 'francais' ; I18n.locale = :en
     r.to_rdf('http://test.uri')
+  end
+  
+  post :index do
+    body = '{"@context":{"dc":"http://purl.org/dc/terms/"},"@id":"http://test.uri","dc:title":[{"@value":"title","@language":"en"},{"@value":"francais","@language":"fr"}]}'
+
+    hash = JSON.parse body rescue return 400 # JSON is mal-formed
+
+    graph = RDF::Graph.new << JSON::LD::API.toRdf(hash)
+    
+    return 422 unless graph.valid? # JSON is well-formed, but JSON-LD is not valid RDF
+    
+    r = Resource.new_from_rdf(graph)
   end
   
   # get :index, :map => '/foo/bar' do
