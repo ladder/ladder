d204d2074eed0369dfa3391adf4f2ca6e1b8ad42
mjsuhonos <mj@suhonos.ca>
2014-04-22 20:01:20 -0400

    Simplify #after_build logic

--- lib/ladder/model/embedded.rb
+++ lib/ladder/model/embedded.rb
@@ -7,16 +7,12 @@ module Ladder
 
       # Once the document is built, bind it to the vocab's properties
       after_build do |document|
-        vocab = RDF::Vocabulary.from_uri(self.to_uri)
+        vocab = RDF::Vocabulary.from_uri(RDF::URI.from_qname metadata[:name])
 
         # Create a Mongoid field for each property
-        vocab.predicates.each do |field|
-          self.class.field field, :type => Array, :localize => true
+        vocab.predicates.each do |field_name|
+          self.class.field field_name, :type => Array, :localize => true
         end
-      end
-
-      def to_uri
-        RDF::URI.from_qname metadata[:name] unless metadata.nil?
       end
 
     end
