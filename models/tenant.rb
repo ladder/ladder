90232aaf02c4a8cc1c312eb37f337d4ed16fb579
mjsuhonos <mj@suhonos.ca>
2014-04-28 20:20:59 -0400

    Clean up Tenant, controller

--- models/tenant.rb
+++ models/tenant.rb
@@ -1,8 +1,8 @@
 class Tenant
   include Mongoid::Document
 
-  field :api_key, type: String
   field :email, type: String
+  field :api_key, type: String
   field :database, type: String
   field :properties, type: Hash, default: {
     models: [
@@ -15,11 +15,9 @@ class Tenant
   after_initialize :generate_api_key
   after_initialize :set_database
 
-  validates_presence_of :api_key
-  validates_presence_of :email
-  validates_presence_of :database
+  validates_presence_of :email, :api_key, :database
 
-  store_in database: 'ladder', collection: 'tenants'
+  store_in database: 'ladder'
 
   def generate_api_key
     # API key is a 32-character random Hex string
