50178b1be8165fbdd4124289afcf73c63e3c56d0
mjsuhonos <mj@suhonos.ca>
2014-04-30 05:05:42 -0400

    Add Tenant#define_models autoload/create

--- models/tenant.rb
+++ models/tenant.rb
@@ -5,6 +5,7 @@ class Tenant
   field :api_key, type: String
   field :database, type: String
   field :properties, type: Hash, default: {
+    # FIXME: TEMPORARY FOR DEBUGGING
     models: [
       {name: 'Resource', vocabs: ['RDF::DC', 'RDF::MODS']},
       {name: 'Concept',  vocabs: ['RDF::SKOS']},
@@ -14,6 +15,9 @@ class Tenant
 
   after_initialize :generate_api_key
   after_initialize :set_database
+
+  after_find :define_models
+  after_create :define_models
 
   validates_presence_of :email, :api_key, :database
 
@@ -27,6 +31,15 @@ class Tenant
   def set_database
     address = Mail::Address.new(self.email)
     self.database ||= address.domain unless address.domain.nil?
+  end
+  
+  # TODO: handle model removal?
+  def define_models
+    return unless self.properties[:models] and self.properties[:models].is_a? Array
+    
+    self.properties[:models].map do |model|
+      Ladder::RDF.model model.merge module: "L#{self.id}"
+    end
   end
 
 end
