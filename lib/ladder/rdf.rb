67ee4bf5882d7721e8540bb7ff2425d1044c48cf
MJ Suhonos <mj@suhonos.ca>
2014-07-10 12:27:58 -0400

    Stricter checking in model generation

--- lib/ladder/rdf.rb
+++ lib/ladder/rdf.rb
@@ -16,16 +16,16 @@ module Ladder
     # :name (String)         -> the name of the model class to create
     # :module (String)       -> the name of a module to namespace classes within
     # :vocabs (Array:String) -> a list of RDF::Vocabulary classes to use
-    # :types (Array:String)  -> a list of pname RDF classes to assign
+    # :types (Array:String)  -> a list of pname RDF classes to assign from given vocabs
 
     def self.model(*args)
       opts = args.last || {}
       opts.symbolize_keys!
 
       return unless name = opts[:name] and mod = opts[:module] and vocabs = opts[:vocabs] and types = opts[:types]
-      return unless mod.is_a? String
-      return unless vocabs.is_a? Array
-      return unless types.is_a? Array
+      return unless mod.is_a? String and ! mod.empty?
+      return unless vocabs.is_a? Array and ! vocabs.empty?
+      return unless types.is_a? Array and ! types.empty?
 
       # If this Module is already defined, use it
       namespace = if Object.const_defined? mod.classify and mod.classify.constantize.is_a? Module
@@ -50,6 +50,9 @@ module Ladder
         term = ::RDF::Vocabulary.expand_pname type
 
         next unless term.is_a? ::RDF::Vocabulary::Term
+        
+        # check type against vocabs for this model
+        next unless klass.vocabs.include? term.vocab.to_uri
 
         # use CamelCase convention to ensure this is a Class property
         next unless term.qname.last.to_s.camelize == term.qname.last.to_s and term.qname.last.to_s.upcase != term.qname.last.to_s
