990803471fd3d4e6f6f2d256bf5e00bc5d881d33
MJ Suhonos <mj@suhonos.ca>
2014-10-03 01:31:19 -0400

    Refactor out dynamic definitions

--- lib/ladder/resource.rb
+++ lib/ladder/resource.rb
@@ -5,13 +5,13 @@ require 'json/ld'
 module Ladder::Resource
   extend ActiveSupport::Concern
 
+  autoload :Dynamic, 'ladder/resource/dynamic'
+
   included do
     include Mongoid::Document
     include ActiveTriples::Identifiable
-    
-    field :_context, type: Hash # for tracking dynamic context
-    
-    after_find :apply_context
+
+    configure base_uri: RDF::URI.new(LADDER_BASE_URI) / name.underscore.pluralize if defined? LADDER_BASE_URI
   end
 
   ##
@@ -34,12 +34,12 @@ module Ladder::Resource
 
       values = objects.map do |obj|
         if obj.is_a?(ActiveTriples::Identifiable)
-          if relation_hash.keys.include?(name)
+          if relation_hash.keys.include? name 
             obj.update_relations
             obj.resource.set_value(relation_hash[name].inverse, self.rdf_subject)
             obj
           else
-            resource.delete([obj.rdf_subject]) if resource.enum_subjects.include? obj.rdf_subject and ! args[:related]
+            resource.delete [obj.rdf_subject] if resource.enum_subjects.include? obj.rdf_subject and ! args[:related]
             obj.rdf_subject
           end
         else
@@ -53,64 +53,21 @@ module Ladder::Resource
     resource
   end
 
-  ##
-  # Dynamic field definition
-  def define(field_name, *args)
-    # Store context information
-    self._context ||= Hash.new(nil)
-    self._context[field_name] = args.first[:predicate].to_s
+  module ClassMethods
+    ##
+    # Default ActiveTriples #property integration
+    #
+    # @see Mongoid::Document
+    def define(field_name, *args)
 
-    create_accessors field_name
+      if class_name = args.first[:class_name]
+        has_and_belongs_to_many field_name, autosave: true, class_name: class_name
+      else
+        field field_name
+      end
 
-    # Update resource properties
-    resource_class.property(field_name, *args)
+      property(field_name, *args)
+    end
   end
-
-  private
-
-    ##
-    # Dynamic field accessors
-    def create_accessors(field_name)
-      define_singleton_method field_name do
-        read_attribute(field_name)
-      end
-
-      define_singleton_method "#{field_name}=" do |value|
-        write_attribute(field_name, value)
-      end
-    end
-    
-    ##
-    # Apply dynamic fields and properties to this instance
-    def apply_context
-      return unless self._context
-
-      self._context.each do |field_name, uri|
-        if term = RDF::Vocabulary.find_term(uri)
-          create_accessors field_name
-          
-          resource_class.property(field_name, predicate: term)
-        end
-      end
-    end
-
-  public
-
-    module ClassMethods
-      ##
-      # Default ActiveTriples #property integration
-      #
-      # @see Mongoid::Document
-      def define(field_name, *args)
-
-        if class_name = args.first[:class_name]
-          has_and_belongs_to_many field_name, autosave: true, class_name: class_name
-        else
-          field field_name
-        end
-
-        property(field_name, *args)
-      end
-    end
 
 end
