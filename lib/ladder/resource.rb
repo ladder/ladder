31f131fd68e0e97df2abeb5f9ce06883900adbdd
MJ Suhonos <mj@suhonos.ca>
2014-10-08 11:00:10 -0400

    Remove dynamic from master branch

--- lib/ladder/resource.rb
+++ lib/ladder/resource.rb
@@ -8,8 +8,6 @@ module Ladder::Resource
   include Mongoid::Document
   include ActiveTriples::Identifiable
 
-  autoload :Dynamic, 'ladder/resource/dynamic'
-
   included do
     configure base_uri: RDF::URI.new(LADDER_BASE_URI) / name.underscore.pluralize if defined? LADDER_BASE_URI
   end
