67b749262813b0b4b3e45bd031446fc92a70b0e5
MJ Suhonos <mj@suhonos.ca>
2014-10-24 15:23:22 -0400

    Add #dump, fix #save, tidy comments

--- lib/ladder/file.rb
+++ lib/ladder/file.rb
@@ -26,20 +26,33 @@ module Ladder::File
     attrs = args.last.symbolize_keys
 
     self.id = attrs[:id] || attrs[:_id] || BSON::ObjectId.new
-    @readable = attrs[:readable] || StringIO.new(attrs[:data].to_s)
+    @readable ||= attrs[:readable] || StringIO.new(attrs[:data].to_s)
     @file = attrs[:file]
   end
 
+  ##
+  # Make save behave like Mongoid::Document as much as possible
   def save
-    @file = self.class.grid.put(@readable, {id: self.id})
+    @file ? @file.save : !! @file = self.class.grid.put(@readable, {id: self.id})
+  end
+
+  ##
+  # Output content of object from stored file or readable input
+  def dump(*opts)
+    @readable.rewind
+    @file ? @file.data(*opts) : @readable.read
   end
 
   module ClassMethods
 
+    ##
+    # Create a namespaced GridFS module for this class
     def grid
      @grid ||= Mongoid::GridFs.build_namespace_for name
     end
     
+    ##
+    # Make find behave like Mongoid::Document as much as possible
     def find(*args)
       id = args.shift unless args.first.is_a? Hash
 
