353813b6c97200851ce4db337cc9475bdc6ab1c3
MJ Suhonos <mj@suhonos.ca>
2014-11-18 15:16:35 -0500

    Stubbing out specs

--- spec/ladder/file_spec.rb
+++ spec/ladder/file_spec.rb
@@ -15,11 +15,15 @@ describe Ladder::File do
 
   context 'with data from file' do
     let(:subject) { Datastream.new File.open('./spec/shared/moomin.jpg') }
+    let(:source) { File.read('./spec/shared/moomin.jpg') }
+
     it_behaves_like 'a File'
   end
 
   context 'with data from string' do
-    let(:subject) { Datastream.new(data: "And so Moomintroll was helplessly thrown out into a strange and dangerous world and dropped up to his ears in the first snowdrift of his experience. It felt unpleasantly prickly to his velvet skin, but at the same time his nose caught a new smell. It was a more serious smell than any he had met before, and slightly frightening. But it made him wide awake and greatly interested.") }
+    data = "And so Moomintroll was helplessly thrown out into a strange and dangerous world and dropped up to his ears in the first snowdrift of his experience. It felt unpleasantly prickly to his velvet skin, but at the same time his nose caught a new smell. It was a more serious smell than any he had met before, and slightly frightening. But it made him wide awake and greatly interested."
+    let(:subject) { Datastream.new(data: data) }
+    let(:source) { data }
 
     it_behaves_like 'a File'
   end
--- spec/shared/file.rb
+++ spec/shared/file.rb
@@ -8,30 +8,31 @@ shared_examples 'a File' do
 
   describe '#initialize' do
     it 'should have an id' do
-#      expect(subject.id).to be_kind_of BSON::ObjectID
-    end
-    
-    it 'should have a stored readable' do
+      expect(subject.id).to be_kind_of BSON::ObjectId
     end
   end
 
   describe '#data' do
     it 'should return a data stream' do
+      expect(subject.data).to eq source
     end
   end
 
   describe '#save' do
     it 'should persist' do
+      expect(subject.save).to be true
     end
   end
 
   describe '#find' do
     it 'should be retrievable' do
+      expect(subject.class.find(subject.id)).to eq subject
     end
   end
 
   describe '#==' do
     it 'should be comparable' do
+      # TODO
     end
   end
 
