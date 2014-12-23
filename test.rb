1f09424fa8f88048fa69d77c328656dc3b06cd70
MJ Suhonos <mj@suhonos.ca>
2014-12-23 14:59:32 -0500

    Update temp test file

--- test.rb
+++ test.rb
@@ -7,14 +7,26 @@ Mongoid.purge!
 
 LADDER_BASE_URI = 'http://example.org'
 
-class Test
+class Person
+  include Ladder::Resource
+
+  configure type: RDF::FOAF.Person
+
+  property :first_name, predicate: RDF::FOAF.name
+
+  embeds_one :thumbnail, class_name: 'Image'
+  property :thumbnail,  predicate: RDF::FOAF.depiction
+end
+
+class Image
   include Ladder::File
 end
 
-t = Test.new data: 'this is a test'
-t.save
-t.save # shouldn't do anything
+steve = Person.new(first_name: 'Steve')
+#steve.thumbnail = Image.new(open('http://www.showbizsandbox.com/wp-content/uploads/2011/08/Steve-Jobs.jpg'))
+#steve.save
 
-t2 = Test.find t.id
+#xml = Image.new(data: '<test>some xml data</test>')
+#json = Image.new(StringIO.new("{'test' : 'some json data'}"))
 
 binding.pry
