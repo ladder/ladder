c490290730e477cab451bd3ba0fe6c9ca7fa9c81
MJ Suhonos <mj@suhonos.ca>
2014-12-25 13:43:27 -0500

    MOAR REFACTOR

--- lib/ladder/file.rb
+++ lib/ladder/file.rb
@@ -1,74 +1,53 @@
-require 'bson'
 require 'mongoid/grid_fs'
 require 'active_triples'
 
 module Ladder::File
   extend ActiveSupport::Concern
 
+  include Mongoid::Document
   include ActiveTriples::Identifiable
 
   included do
     configure base_uri: RDF::URI.new(LADDER_BASE_URI) / name.underscore.pluralize if defined? LADDER_BASE_URI
+    
+    store_in :collection => "#{ grid.prefix }.files"
   end
 
-  delegate :_id, :id, to: :@file
-  delegate *Mongoid::Atomic.instance_methods, to: :@file
-  delegate *Mongoid::Changeable.instance_methods, to: :@file
-  delegate *Mongoid::Document.instance_methods, to: :@file
-  delegate *Mongoid::Relations.instance_methods, to: :@file
-  delegate *Mongoid::Stateful.instance_methods, to: :@file
-  delegate *Mongoid::Traversable.instance_methods, to: :@file
-  delegate *Mongoid::Validatable.instance_methods, to: :@file
-=begin
-  def compact
-    self
-  end
+  attr_accessor :file
 
-  def map(&block)
-    yield self
-  end
-=end  
-  ##
-  # Make constructor as ActiveModel-like as possible
-  #
-  # eg. Ladder::File.new(StringIO.new)
-  #     Ladder::File.new(data: '... binary data ...')
-  #
   def initialize(*args)
-    if args.first.is_a? Hash
-      self.data = args.first[:data].to_s
-    else
-      @readable = args.first
+    super
+    
+    # If we are loading an existing GridFS file, populate values
+    if file.respond_to?(:data)
+      @grid_file = file
+      self.id = file.id
     end
-
-    @file = @readable.respond_to?(:data) ? @readable : self.class.grid::File.new
   end
-  
+
   ##
   # Make save behave like Mongoid::Document as much as possible
   def save
-    # TODO: clean up logic here
-    return false if @readable.nil? or 0 == @readable.size
-    @file.persisted? ? @file.save : !! @file = self.class.grid.put(@readable)
+    return false if file.nil? or 0 == file.size
+
+    @grid_file ? @grid_file.save : @grid_file = self.class.grid.put(file)
+    self.id = @grid_file.id
+
+    true
   end
 
   ##
   # Output content of object from stored file or readable input
   def data(*opts)
-    @file.persisted? ? @file.data(*opts) : @readable.read
-  end
-  
-  ##
-  # Allow setting data on existing object
-  def data=(string)
-    @readable = StringIO.new(string)
+    if @grid_file
+      @grid_file.data(*opts)
+    else
+      file.rewind if file.respond_to? :rewind
+      file.read
+    end
   end
 
   module ClassMethods
-
-    delegate :all, :all_of, :relations, :where, to: :'grid::File'
-    delegate *Mongoid::Relations::Reflections.instance_methods(false), to: :'grid::File'
-    delegate *Mongoid::Relations::Referenced::Many.methods(false), to: :'grid::File'
 
     ##
     # Create a namespaced GridFS module for this class
@@ -82,7 +61,7 @@ module Ladder::File
       id = args.shift unless args.first.is_a? Hash
       file = id ? grid.get(id) : grid.find(*args)
 
-      self.new(file)
+      self.new(file: file)
     end
 
   end
--- test.rb
+++ test.rb
@@ -14,9 +14,7 @@ class Person
   configure type: RDF::FOAF.Person
 
   property :first_name, predicate: RDF::FOAF.name
-
-  has_one :thumbnail, class_name: 'Image'
-  property :thumbnail,  predicate: RDF::FOAF.depiction
+  property :thumbnails,  predicate: RDF::FOAF.depiction, class_name: 'Image'
 end
 
 class Image
@@ -24,51 +22,11 @@ class Image
 end
 
 steve = Person.new(first_name: 'Steve')
-i = Image.new(open('http://www.showbizsandbox.com/wp-content/uploads/2011/08/Steve-Jobs.jpg'))
-steve.thumbnail = i
 
-steve.save
-steve.save
-
-i.save
-i.save
-
-#Ladder::File.create :Image
-#i = Image.new
-
-#xml = Image.new(data: '<test>some xml data</test>')
-#json = Image.new(StringIO.new("{'test' : 'some json data'}"))
+i = Image.new(file: open('http://www.showbizsandbox.com/wp-content/uploads/2011/08/Steve-Jobs.jpg'))
 
 binding.pry
 
-=begin
-  module ClassMethods
-
-    ##
-    # Create a namespaced GridFS module for this class
-    def grid
-     @grid ||= Mongoid::GridFs.build_namespace_for name
-    end
-
-  end
-
-  ##
-  # Factory creation method
-  def self.create(class_name, &block)
-    ns = Mongoid::GridFs.build_namespace_for class_name
-
-    klass = Class.new(ns::File) do
-      include ActiveTriples::Identifiable
-      include InstanceMethods
-      extend ClassMethods
-
-      configure base_uri: RDF::URI.new(LADDER_BASE_URI) / name.underscore.pluralize if defined? LADDER_BASE_URI
-      @grid = ns
-
-      class_eval(&block) if block_given?
-    end
-
-    Object.const_set(class_name, klass)
-  end
-
-=end
+# steve.save
+# i.save
+# steve.thumbnails << i
