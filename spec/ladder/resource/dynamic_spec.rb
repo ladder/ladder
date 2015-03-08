e7287fdf6c545f745b8b1a8b175f4f70b8f8cbee
MJ Suhonos <mj@suhonos.ca>
2015-03-07 22:31:08 -0500

    Use new context in dynamic spec

--- spec/ladder/resource/dynamic_spec.rb
+++ spec/ladder/resource/dynamic_spec.rb
@@ -16,13 +16,7 @@ describe Ladder::Resource::Dynamic do
   context 'with data' do
     let(:subject) { Thing.new }
 
-    before do
-      # non-localized literal
-      subject.alt = 'Mumintrollet pa kometjakt'
-
-      # localized literal
-      subject.title = 'Comet in Moominland'
-    end
+    include_context 'with data'
 
     it_behaves_like 'a Resource'
     it_behaves_like 'a Dynamic Resource'
