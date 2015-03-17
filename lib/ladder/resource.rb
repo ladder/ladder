2355fa2d1c8b7d2e14c2e0734422e882975ae854
MJ Suhonos <mj@suhonos.ca>
2015-03-17 16:20:44 -0400

    Add #base_uri method

--- lib/ladder/resource.rb
+++ lib/ladder/resource.rb
@@ -280,20 +280,27 @@ module Ladder
         new_object
       end
 
-      protected
-
       ##
       # Set a default base URI based on the Ladder::Config settings
       #
+      # @return [RDF::URI]
+      def base_uri
+        RDF::URI.new(Ladder::Config.settings[:base_uri]) / name.underscore.pluralize
+      end
+
+      protected
+
+      ##
+      # Register with Ladder and set the default base URI
+      #
       # @return [void]
       def configure_model
-        # TODO: extract to configurable
+        configure base_uri: base_uri
         Ladder::Config.register_model self unless Ladder::Config.models.include? self
-        configure base_uri: RDF::URI.new(Ladder::Config.settings[:base_uri]) / name.underscore.pluralize
       end
 
       ##
-      # Propagate base uri and properties to subclasses
+      # Propagate base URI and properties to subclasses
       #
       # @return [void]
       def inherited(subclass)
